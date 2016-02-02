a015e708e6cf743b070b089258291115f9487b1c
Joseph McClenahan <jmcclena94@gmail.com>
2016-02-01 21:09:21 -0800

    updated html and css and linked js with jquery

---
+++ scripts/app.js
@@ -0,0 +1,51 @@
+(function(){
+  'use strict';
+})();
+
+var blogs = [];
+
+function blog (data) {
+  this.author = data.author;
+  this.bio = data.bio
+  this.publishedOn = data.publishedOn;
+  this.title = data.title;
+  this.category = data.category;
+}
+
+blog.prototype.toHtml = function() {
+  var $newBlog = $('article.template').clone();
+
+  $newBlog.attr('data-category',this.category);
+
+  $newBlog.find('h1').text(this.title);
+  $newBlog.find('h3').text(this.author);
+  $newBlog.find('time[pubdate]').attr('title',this.publishedOn);
+  $newBlog.find('p').text(this.bio);
+
+  $newBlog.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago.');
+
+  $newBlog.append('<hr>');
+
+  $newBlog.removeClass();
+  $newBlog.addClass('blogPost');
+
+  return $newBlog;
+}
+
+blogData.forEach(function(ele) {
+  blogs.push(new blog(ele));
+})
+
+blogs.forEach(function(a){
+  $('#blog').append(a.toHtml());
+});
+
+// $('#blogLink').hover(function(){
+//   $(this).css('curson','pointer');
+// });
+//
+// $('#blogLink').click(function(){
+//   blogs.forEach(function(a) {
+//     $('#content').append(a.toHtml());
+//   });
+// });

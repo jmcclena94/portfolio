a622763a65091bc90c69b65824ae6914d85a7c9a
Joseph McClenahan <jmcclena94@gmail.com>
2016-02-03 20:47:06 -0800

    added an image, updated dynamic sizing, added a portfolio item.

--- scripts/page_data.js
+++ scripts/page_data.js
@@ -6,7 +6,7 @@
 var blogData = [{
   author: 'Joe McClenahan',
   title: 'About Me',
-  bio: 'I am originally from Bellingham Washington.  I attended school at Western Washington University where I received a degree in Geophysics and Mathematics.  I went on to study geophysics at the University of Wyoming.  For the last three years I have worked as a Geophysicist for an oil services company in Houston, TX.  I am currently attending Code Fellows in Seattle, WA to pursue a development career.',
+  bio: '<img src="img/about_pic.png">I am originally from Bellingham Washington.  I attended school at Western Washington University where I received a degree in Geophysics and Mathematics.  I went on to study geophysics at the University of Wyoming.  For the last three years I have worked as a Geophysicist for an oil services company in Houston, TX.  I am currently attending Code Fellows in Seattle, WA to pursue a development career.',
   publishedOn: '2016-02-01',
   category: 'about',
 }];
@@ -32,10 +32,15 @@ var socData = [{
     link: 'https://www.linkedin.com/in/josephmcclenahan',
 }];
 
-var portfolioData = [{
+var portfolioData = [
+{
   name:'Drink With a Cold One',
   url: 'http://lwenke01.github.io/virtual-drunks-project/',
   description:'A virtual drinking game where you drink with a deceased historical figure.  They can insult you using their own quotations.  This game was created as a Code Fellows 201 final project.',
+},
+{
+  name:'Geophysics Thesis Work',
+  url:'https://gsa.confex.com/gsa/2012RM/finalprogram/abstract_203443.htm',
+  description:'Thesis work on mid-lithospheric discontinuities in the Western United States.'
 }
-
 ];

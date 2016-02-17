19477f16176778334d880bc34d3aeb98504f4324
Joseph McClenahan <jmcclena94@gmail.com>
2016-02-17 15:33:06 -0800

    added procfile, updated gitignore, changed repos.js to link to shell variable

--- scripts/repos.js
+++ scripts/repos.js
@@ -4,15 +4,20 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-       url: 'https://api.github.com/users/jmcclena94/repos' +
-             '?per_page=5&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data, message, xhr) {
-         repos.all = data;
-       }
-     }).done(callback);
+    $.get('https://api.github.com/users/jmcclena94/repos?per_page=5&sort=updated')
+      .done(function(data, message, xhr) {
+        repos.all=data;
+      })
+      .done(callback);
+    // $.ajax({
+    //    url: 'https://api.github.com/users/jmcclena94/repos' +
+    //          '?per_page=5&sort=updated',
+    //    type: 'GET',
+    //    headers: { 'Authorization': 'token ' + githubToken },
+    //    success: function(data, message, xhr) {
+    //      repos.all = data;
+    //    }
+    //  }).done(callback);
   };
 
   repos.with = function(attr) {
